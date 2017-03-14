f9507c5da66805e889178e4af5b8a2b6d9ceea36
Jeng <jeng.tam@udfex.com>
2017-03-14 21:03:24 +0800

    分类开发

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,25 @@ var sh = require('shelljs');
 var webserver = require('gulp-webserver');
 var url = require('url');
 var mockApi = require('./mock');
+var fs = require('fs');
+var path = require('path');
+var livereload = require('gulp-livereload');
+var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var requirejsOptimize = require('gulp-requirejs-optimize');
+var $ = require("gulp-load-plugins")();
+var imagemin = require('gulp-imagemin');
+//var usemin = require('gulp-usemin');
+var sourcemaps = require('gulp-sourcemaps');
+var copy = require('gulp-copy');
+var htmlmin = require('gulp-htmlmin');              //html压缩
+var notify = require('gulp-notify');                //任务通知
+var minifycss = require('gulp-minify-css');         //css压缩
+var jshint = require('gulp-jshint');              //js检测
+var csslint = require('gulp-csslint');              //css检测
+var rev = require('gulp-rev');                      //对文件名加MD5后缀
+var revCollector = require('gulp-rev-collector');   //路径替换
+var gulpsync = require('gulp-sync')(gulp);
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -57,7 +76,7 @@ gulp.task('git-check', function(done) {
 //web服务器
 gulp.task('server', function() {
   var environment = require("./environments.json");
-  var ENV = environment['development'];
+  var ENV = environment['production'];
   gulp.src(ENV.serverPath) // 服务器目录（./代表根目录）
     .pipe(webserver({ // 运行gulp-webserver
       port: ENV.port, //端口，默认8000
@@ -82,3 +101,132 @@ gulp.task('server', function() {
       open: 'http://localhost:' + ENV.port + '/index.html'
     }));
 });
+
+
+// clean
+gulp.task('clean', function(){
+  return gulp.src(['build/**', 'rev/**', 'temp/**'], {read: false})
+    .pipe(clean());
+});
+
+// js检查
+gulp.task('jshint', function () {
+  gulp.src('./www/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// css检查
+gulp.task('csslint', function (cb) {
+  var output = '';
+  gulp.src('./www/css/**/*.css')
+    .pipe(csslint())
+    .pipe(csslint.formatter('junit-xml', {logger: function(str) { output += str; }}))
+    .on('end', function(err) {
+      if (err) return cb(err);
+      fs.writeFile('csslint_report.xml', output, cb);
+    });
+});
+
+// html压缩
+gulp.task('htmlmin', function() {
+  return gulp.src(['./www/**/*.html', '!www/lib/**'])
+    .pipe(gulp.dest('build'))
+    .pipe(notify({ message: 'html min task ok' }));
+
+});
+
+// fonts
+gulp.task('fonts', function() {
+  return gulp.src(['www/fonts/**'])
+    .pipe(gulp.dest('build/fonts'))
+    .pipe(notify({ message: 'fonts task ok' }));
+
+});
+
+//css处理
+gulp.task('css', function(){
+  return gulp.src('temp/css/*.css')            //设置css
+    .pipe(concat('product.css'))            //合并css文件
+    .pipe(gulp.dest('build/css'))           //设置输出路径
+    .pipe(rename({suffix:'.min'}))          //修改文件名
+    .pipe(minifycss())                      //压缩文件
+    //.pipe(rev())                            //生成MD5指纹
+    .pipe(gulp.dest('build/css'))           //输出文件目录
+    //.pipe(rev.manifest({
+    //    merge: true // merge with the existing manifest (if one exists)
+    //}))                   //- 生成一个rev-manifest.json
+    //.pipe(gulp.dest('./rev'))               //- 将 rev-manifest.json 保存到 rev 目录内
+    .pipe(notify({message:'css task ok'})); //提示成功
+});
+
+// images
+gulp.task('img', function () {
+  return gulp.src('www/img/*.{png,jpg,gif,ico}')
+    .pipe(imagemin({
+      optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+      progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+      interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+      multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+    }))
+    .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('www/index.html')
+    .pipe($.usemin({
+      jsAttributes : {
+        'data-main' : 'main'
+      },
+      css: [$.minifyCss()]
+    }))
+    .pipe(gulp.dest('temp'));
+});
+
+// rev md5指纹替换
+gulp.task('rev-md', function() {
+  return gulp.src(['build/**/*.js', 'build/**/*.css'])
+    .pipe(rev())
+    .pipe(gulp.dest('build'))  // write rev'd assets to build dir
+    .pipe(rev.manifest())                   //- 生成一个rev-manifest.json
+    .pipe(gulp.dest('./rev'));
+});
+
+gulp.task('rev',['rev-md'], function(){
+  return gulp.src(['./rev/rev-manifest.json', './temp/index.html'])   //- 读取 rev-manifest.json 文件以及需要进行css名替换的文件
+    .pipe(revCollector())                                   //- 执行文件内css名的替换
+    .pipe(gulp.dest('build'));                     //- 替换后的文件输出的目录
+});
+
+// rjs
+gulp.task('rjs', function () {
+  return gulp.src('www/main.js')
+    .pipe(sourcemaps.init())
+    .pipe(requirejsOptimize({
+      mainConfigFile:"www/config.js",
+      optimize: 'none',
+      generateSourceMaps:true
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build'));
+});
+
+// js
+gulp.task('js', function(){
+  return gulp.src([
+    'www/lib/requirejs/require.js',
+    'build/app.js'])
+    .pipe(concat('product.js'))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(uglify())
+    .pipe(rename({suffix:'.min'}))          //修改文件名
+    //.pipe(rev())                            //生成MD5指纹
+    .pipe(gulp.dest('./build/js'))
+    //.pipe(rev.manifest({
+    //    merge: true // merge with the existing manifest (if one exists)
+    //}))                   //- 生成一个rev-manifest.json
+    //.pipe(gulp.dest('./rev'))               //- 将 rev-manifest.json 保存到 rev 目录内
+    .pipe(notify({message:'js task ok'})); //提示成功
+});
+
+gulp.task('default',gulpsync.sync(['clean', 'html', 'css', 'rjs', 'js', 'img', 'htmlmin', 'fonts', 'rev']));
--- www/modules/product/product.js
+++ www/modules/product/product.js
@@ -2,7 +2,7 @@
  * Created by tanxinzheng on 17/3/3.
  */
 define(function(){
-  return ['$scope', 'ProductAPI', function($scope, ProductAPI){
+  return ['$scope', 'ProductAPI', 'ClassService', '$ionicSlideBoxDelegate', function($scope, ProductAPI, ClassService, $ionicSlideBoxDelegate){
     $scope.getAddressInfo = function(){
       ProductAPI.query({
         pageSize:10,
@@ -11,6 +11,33 @@ define(function(){
         $scope.address = data.data;
       })
     };
+    //初始化
+    $scope.firstClass = ClassService.getFirstClass();
+    $scope.secondClass = ClassService.getSecondClass($scope.firstClass[0].name);
+    $scope.thirdClass = ClassService.getThirdClass($scope.secondClass[0].name);
+    $scope.text = $scope.thirdClass[0].name;
+    $scope.slideIndex = 0;
+    $scope.slideSecondIndex = 0;
+    $scope.slideThirdIndex = 0;
+
+
+    $scope.firstClick = function (index,item,event) {
+      $scope.slideIndex = index;
+      $scope.secondClass = ClassService.getSecondClass(item.name)
+      $scope.slideSecondIndex = 0;
+      $scope.thirdClass = ClassService.getThirdClass($scope.secondClass[0].name);
+      $scope.slideThirdIndex = 0
+      $scope.text = $scope.thirdClass[0].name;
+      //event.target.style.border = "2px solid blue"
+    };
+    $scope.secondClick = function (index,item){
+      $scope.slideSecondIndex = index;
+      $scope.thirdClass = ClassService.getThirdClass(item.name);
+    }
+    $scope.thirdClick = function (index,item) {
+      $scope.slideThirdIndex = index;
+      $scope.text = item.name;
+    }
     var init = function(){
       $scope.getAddressInfo();
     };
